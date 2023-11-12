1bd67e75f1a6fdfe92bbf2ea3c5c35e3faa3a832
ken <1365884335@qq.com>
2023-11-12 23:25:35 +0800

    已更正

--- python进阶/2.函数加减.py
+++ python进阶/2.函数加减.py
@@ -1,4 +1,9 @@
 def xy(x, y):
     data = x + y
-    print(f"{x}+{y}={x + y}")
-xy(8, 5)
+    print(f"{x}+{y}={data}")
+
+
+addend = int(input("请输入第一个数："))
+added = int(input("请输入第二个数："))
+
+xy(addend, added)
