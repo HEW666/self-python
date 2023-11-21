27fc247e34eb80b2143122fd9cb415f6c26e22c6
ken <1365884335@qq.com>
2023-11-21 21:43:08 +0800

    新学习

---
+++ 3.数据容器/列表的定义.py
@@ -0,0 +1,17 @@
+# 列表的基础
+hello = ["hello", "world"]
+print(hello)
+
+# 不同类型的字符的类型
+date = [123, "类型", True]
+print(date)
+print(type(date))
+
+# 嵌套
+code = [["你好"], ["世界"]]
+print(code)
+print(type(code))
+
+# 嵌套列表的索引
+data = [["home", "hello"], ["yellow", "black"]]
+print(data[1][0])
