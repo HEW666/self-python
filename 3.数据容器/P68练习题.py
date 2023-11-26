141b61ecae47f0c3b45bfaeff5f81745820f9661
ken <1365884335@qq.com>
2023-11-26 23:30:45 +0800

    新学习的知识练习题

---
+++ 3.数据容器/P68练习题.py
@@ -0,0 +1,15 @@
+data = ('周杰伦', 11, ['football', 'music'])
+
+# 查询其年龄所在的下标位置
+print(f"1. {data.index(11)}")
+
+# 查询学生的姓名
+print(f"2. {data[0]}")
+
+# 删除学生爱好中的football
+del data[2][1]
+print(data)
+
+# 增加爱好:coding到爱好list内
+data[2].append("coding")
+print(data)
