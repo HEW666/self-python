52c2001ece95f0f1ec2bf991e4213ff2de315931
ken <1365884335@qq.com>
2023-11-27 22:08:14 +0800

    新学习

---
+++ 3.数据容器/字符串.py
@@ -0,0 +1,9 @@
+data = "itheima itcast boxuegu"
+num = data.count("it")
+print(f"it出现次数{num}")  # it出现的次数
+
+name = data.replace(" ", "|")
+print(f"替换后{name}")  # 将空替换为|
+
+sum = data.split(" ")
+print(sum)  # 输出列表
