5846ef6eefb382b9fed9a912428623807a489ef7
ken <1365884335@qq.com>
2023-12-13 22:06:41 +0800

    学习模块

---
+++ 7.模块/模块的导入.py
@@ -0,0 +1,24 @@
+# import time
+# print("延迟5秒")
+# time.sleep(5)
+# print("延迟五秒后")
+
+# from time import sleep
+# print("延迟5秒")
+# sleep(5)
+# print("延迟五秒后")
+
+# from time import *
+# print("延迟5秒")
+# sleep(5)
+# print("延迟五秒后")
+
+# import time as tx
+# print("延迟5秒")
+# tx.sleep(5)
+# print("延迟五秒后")
+
+from time import sleep as xt
+print("延迟5秒")
+xt(5)
+print("延迟五秒后")
