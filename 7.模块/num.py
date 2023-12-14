0bb4f4bcbefd483e581f1ca97a91776dc5755736
ken <1365884335@qq.com>
2023-12-14 19:59:23 +0800

    学习模块

---
+++ 7.模块/num.py
@@ -0,0 +1,14 @@
+__all__ = ['add']  # 引用带*只能使用此变量
+
+
+def add(a, b):
+    print(a + b)
+
+
+def jj(a, b):
+    print(a - b)
+
+
+if __name__ == "__main__":
+    print("55")
+    # 只有程序直接执行才会进入
---
+++ 7.模块/自定义模块.py
@@ -0,0 +1,10 @@
+# import num  # 导入
+# num.add(5, 3)  # 使用
+
+from num import *
+
+add(6, 5)
+
+from num import jj
+
+jj(3, 3)
