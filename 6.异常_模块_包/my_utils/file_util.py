c9300600bffc0edc42b9dcd7b7eb67356e4e65c7
ken <1365884335@qq.com>
2023-12-19 23:10:27 +0800

    完善

--- 6.异常_模块_包/my_utils/file_util.py
+++ 6.异常_模块_包/my_utils/file_util.py
@@ -1,15 +1,24 @@
 # 文件处理
+
 def print_file_info(file_name):
+    """
+    将文件内容输出
+    :param file_name:
+    :return: 文件内容
+    """
+    f = None
     try:
         f = open(file_name, "r", encoding="UTF-8")
         print(f.read())
-        f.fileno()
     except FileNotFoundError as a:
         print(f"文件不存在：{a}")
+    finally:
+        if f:  # 如果变量是None，表示False，如果有任何内容，就是True
+            f.close()  # 有内容就执行
 
 
 def append_to_file(file_name, data):
     f = open(file_name, "a", encoding="UTF-8")
     f.write(data)
+    f.write("\n")
     f.close()
-
