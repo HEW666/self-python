e8044109baed1ffd97ebe7e89273f1b3b8b8614c
ken <1365884335@qq.com>
2024-03-10 21:27:10 +0800

    MySQL被Python操作

---
+++ 8.MySQL数据库/连接MySQL.py
@@ -0,0 +1,20 @@
+from pymysql import Connection
+
+# 构建数据库的连接
+conn = Connection(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="123456"
+)
+print(conn.get_server_info())  # 返回数据库版本
+
+conn.select_db("world")  # 选择数据库
+cursor = conn.cursor()  # 使用cursor类
+cursor.execute("select * from student")  # 查询数据库指令
+
+results = cursor.fetchall()  # 接受返回数据
+
+for r in results:
+    # 遍历输出
+    print(r)
