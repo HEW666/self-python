1e27eb3a21878ec1e18118f0fe771e62de84bb75
ken <1365884335@qq.com>
2024-03-10 22:12:44 +0800

    数据库插入.py

---
+++ 8.MySQL数据库/数据库插入.py
@@ -0,0 +1,21 @@
+from pymysql import Connection
+
+# 构建数据库的连接
+conn = Connection(
+    host="localhost",   # 主机名（IP）
+    port=3306,          # 端口
+    user="root",        # 账户
+    password="123456",  # 密码
+    autocommit=True     # 自动提交（确认）
+)
+# 获取到游标对象
+cursor = conn.cursor()
+# 选择数据库
+conn.select_db("world")
+# 执行sql
+cursor.execute("insert into student values(21, '周雅', 24, '女')")
+# 通过commit确认。
+# 也可以在 Connection 里写入 autocommit=True
+# conn.commit()
+# 关闭链接
+conn.close()
